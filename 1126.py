1c244f08f2c285bc3d1a83fd4888f161e742cee4
sirmeenew <69440879+sirmeenew@users.noreply.github.com>
2024-11-26 06:20:51 +0900

    Update 1126.py

--- 1126.py
+++ 1126.py
@@ -4,41 +4,39 @@ import geopandas as gpd
 import folium
 from streamlit_folium import folium_static
 
-# CSV 파일 로드 (출산율 데이터)
+# CSV 파일 불러오기
 df = pd.read_csv('./연령별_출산율_및_합계출산율_행정구역별__20241126033905.csv', header=1)
 
-# GeoJSON 파일 로드 (행정구역 데이터)
+# GeoJSON 파일 불러오기
 gdf_sigungu = gpd.read_file('./N3A_G0100000.json')
 
-# 지도 설정 (대한민국 중심)
+# 대한민국 중심 좌표
 namhan_center = [36.34, 127.77]
 
-# 지도 제목
+# 지도 생성
 title = '전국 시군구별 합계출산율 지도'
 title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
 
-# Folium 지도 생성
 gu_map = folium.Map(
     location=namhan_center,
     zoom_start=8,
     tiles='cartodbpositron'
 )
 
-# 제목 추가
 gu_map.get_root().html.add_child(folium.Element(title_html))
 
-# Choropleth Layer 추가
+# Choropleth 추가
 folium.Choropleth(
     geo_data=gdf_sigungu,
     data=df,
-    columns=('행정구역별', '합계출산율 (가임여성 1명당 명)'),  # 데이터 컬럼명
-    key_on='feature.properties.NAME',  # GeoJSON 속성 매칭
-    fill_color='BuPu',  # 색상 설정
+    columns=('행정구역별', '합계출산율 (가임여성 1명당 명)'),
+    key_on='feature.properties.NAME',
+    fill_color='BuPu',
     fill_opacity=0.7,
     line_opacity=0.5,
-    legend_name='전국 시군구별 합계출산율'
+    legend_name='전국 시군구별 합계출산율 지도'
 ).add_to(gu_map)
 
-# Streamlit에 Folium 지도 렌더링
-st.title("전국 시군구별 합계출산율")
+# Streamlit에서 Folium 지도 렌더링
 folium_static(gu_map)
+
